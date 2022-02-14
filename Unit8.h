369942df9277cb0f9a08b9ad506e0c11719c9c5a
mishaniahomi <69090763+mishaniahomi@users.noreply.github.com>
2022-02-13 18:16:33 -0800

    Create Unit8.h

---
+++ Unit8.h
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit8H
+#define Unit8H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.jpeg.hpp>
+//---------------------------------------------------------------------------
+class TForm8 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+	TImage *Image2;
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall FormResize(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm8(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm8 *Form8;
+//---------------------------------------------------------------------------
+#endif
